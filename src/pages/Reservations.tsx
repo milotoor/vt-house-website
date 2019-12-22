eb0f7648c96b24f120d444ce2993255c2cab5d50
Milo Toor <milo.toor@gmail.com>
2019-12-22 11:12:24 -0500

    Basic "Reservations" page

--- src/pages/Reservations.tsx
+++ src/pages/Reservations.tsx
@@ -1,2 +1,17 @@
 import React from 'react';
-export default () => <span>Reservations</span>;
+import { Typography } from 'antd';
+
+const { Paragraph, Text, Title } = Typography;
+
+export default () => 
+  <Typography>
+    <Title level={3}>Thank you for your interest!</Title>
+    <Paragraph>
+      To make a reservation, please e-mail us at <a href="mailto:reservations@champlainhaven.com">reservations@champlainhaven.com</a>.
+    </Paragraph>
+
+    <Paragraph>
+      <div><Text strong>Thank you,</Text></div>
+      <div><Text strong>John & Margaret Toor</Text></div>
+    </Paragraph>
+  </Typography>
