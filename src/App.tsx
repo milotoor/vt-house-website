dda63d0ac198a52e6a6e142c7f5e170bb1018637
Milo Toor <milo.toor@gmail.com>
2020-01-24 15:16:24 -0800

    Remove header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,17 @@ import Menu from './Menu';
 import * as Pages from './pages';
 import * as routes from './routes';
 
-const { Header, Content, Footer, Sider } = Layout;
 
-const App = () =>
+const { Content, Sider } = Layout;
+
+const App: React.FC = () =>
   <Router>
     <Layout style={{ minHeight: '100vh' }}>
       <Sider>
         <Menu />
       </Sider>
+
       <Layout>
-        <Header style={{ background: '#fff', padding: 0 }}>
-          Do I have anything to say in the header?
-        </Header>
         <Content style={{ margin: '0 16px' }}>
           <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
           <Switch>
@@ -46,7 +45,6 @@ const App = () =>
           </Switch>
           </div>
         </Content>
-        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
       </Layout>
     </Layout>
   </Router>;
